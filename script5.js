196f545f06ddb9a5996f6428504d6cc206468e75
Thom96 <43645360+Thom96@users.noreply.github.com>
2021-06-10 21:58:49 +0200

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,24 @@
+const makePizza = function () {
+    console.log("Roll the dough");
+    console.log("Add some tomato sauce");
+    console.log("Add vegetables");
+    console.log("Add some chicken or tuna");
+    console.log("top off with cheese");
+    console.log("Put in oven of 240 degrees for 4 minutes");
+    console.log("Serve the pizza!");
+}
+
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log("Put rice on the sushi roll");
+    console.log("Fill the sushi");
+    console.log("Roll the sushi");
+    console.log("Cut in pieces");
+    console.log("Sushi ready to serve!");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
